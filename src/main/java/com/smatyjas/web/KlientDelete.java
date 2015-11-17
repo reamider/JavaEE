e58fe92cfe5f0d69b45cdd732b99bdb4d2cdbfb8
Sebastian <sebastian.matyjas@gmail.com>
2015-11-17 21:28:09 +0100

    POCZATEK PLIKU DELETE KLIENT
    POCZATEK PLIKU DELETE KLIENT

--- src/main/java/com/smatyjas/web/KlientDelete.java
+++ src/main/java/com/smatyjas/web/KlientDelete.java
@@ -1,5 +1,20 @@
 package main.java.com.smatyjas.web;
 
-public class KlientDelete {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import main.java.com.smatyjas.service.KlientManager;
+import java.io.IOException;
+import javax.servlet.annotation.*;
 
+@WebServlet(urlPatterns = "/deleteKlient")
+public class KlientDelete extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		 response.setContentType("text/html");
+		 int index = -1;
+	     int ID = Integer.parseInt(request.getParameter("delete"));
+	}
 }

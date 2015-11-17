d96b3946fd45719073617c47cddb2a734ad9da9b
Sebastian <sebastian.matyjas@gmail.com>
2015-11-17 17:10:46 +0100

    DODANIE METODY DODAJBILET W MANAGERZE
    DODANIE METODY DODAJBILET W MANAGERZE

--- src/main/java/com/smatyjas/service/KlientManager.java
+++ src/main/java/com/smatyjas/service/KlientManager.java
@@ -8,8 +8,10 @@ import main.java.com.smatyjas.domain.Sprzedaz;
 
 public class KlientManager {
 	private List<Klient> klienci = new ArrayList<Klient>();
+	private List<Bilet> bilety = new ArrayList<Bilet>();
 	private List<Sprzedaz> sprzedaze = new ArrayList<Sprzedaz>();
 	private int id_klient = 0;
+	private int id_bilet = 0;
 	private int id_przedstawienie = 0;
 	
 	public void dodajKlienta(Klient klient) {
@@ -17,5 +19,12 @@ public class KlientManager {
 		id_klient++;
 		nowyKlient.setId_klient(id_klient);
 		klienci.add(nowyKlient);
+	}
+	
+	public void dodajBilet(Bilet bilet) {
+		Bilet nowyBilet = new Bilet(bilet.getRodzaj(), bilet.getCena(), bilet.getOpis());
+		id_bilet++;
+		nowyBilet.setId_bilet(id_bilet);
+		bilety.add(nowyBilet);
 	}
 }

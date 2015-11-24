9adcd42cae1f508a9957ccc35df2aaa624b81b53
Sebastian <sebastian.matyjas@gmail.com>
2015-11-24 20:50:17 +0100

    Poprawienie servletów
    Poprawienie servletów - błędy jetty

--- src/main/java/com/smatyjas/web/KlientDelete.java
+++ src/main/java/com/smatyjas/web/KlientDelete.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class KlientDelete extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
-	            {
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");

fc35916388a46b1b8c35518231c0a14a4c19058a
Sebastian <sebastian.matyjas@gmail.com>
2015-11-24 21:28:00 +0100

    Skończony plik edytuj Klienta
    Skończony plik edytowania Klienta z wykorzystaniem servletu

---
+++ src/main/webapp/edytujKlienta.jsp
@@ -0,0 +1,71 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"  import="main.java.com.smatyjas.domain.Klient"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link href="style.css" rel="stylesheet" type="text/css">
+<title>Edytuj Klienta</title>
+</head>
+
+<style rel="stylesheet" type="text/css">
+	body{
+		background-color: #000000;
+		background: url('http://www.tapeta-czarne-linie-tlo.na-telefon.org/tapety/czarne-linie-tlo.jpeg'); 
+		background-position: center top; no-repeat;
+	}
+	.przedrostki{ 
+		margin-top: 4cm; 
+		margin-left: 15cm; 
+		font-size: 24px;
+		color: green;
+	}
+	.zapisz{ 
+		margin-left:100px; 
+		margin-top: 20px;
+	}
+	a{ 
+		color:green; 
+		font-size:24px; 
+		font-weight:bold; 
+		margin-left: 120px;
+		margin-right:auto;
+		margin-top: 2cm;
+	}
+    h1{
+    	margin-left: 16cm;
+    	color: green; 
+    	font-size: 40px; 
+    	font-style: italic;
+        font-family: "Times-New-Roman"; 
+        margin-top: -8cm;
+	}
+</style>
+
+<body>
+	<jsp:useBean id="klient" class="main.java.com.smatyjas.domain.Klient" scope="session" />
+	<jsp:setProperty name="klient" property="*" />
+	<jsp:useBean id="klientmanager" class="main.java.com.smatyjas.service.KlientManager" scope="application" />
+	<%
+        String ID = request.getParameter("update");
+     	int numertelefonu = 0;
+    	String  imie = "",  nazwisko = "";
+        for (Klient k : klientmanager.pobierzKlientow()){
+          if (k.getId_klient() == Integer.parseInt(ID)){
+      		imie = k.getImie();
+       		nazwisko = k.getNazwisko();
+        	numertelefonu = k.getNumertelefonu();
+        	break;
+        	}
+        }
+   		out.println("<div class = 'przedrostki'><form action='updateKlient'><input type='hidden' name='id' value='" + ID +
+		   "' /><tr><td colspan='2'><b>ID Klienta:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;  &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  " + ID +
+		   "</td></tr><br><tr><td><b>Imie: </b> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; </td><td><input type='text' name='imie' pattern='[A-Za-z]*' title='Imie ma powinno składać się tylko z liter' value='" + imie +
+		   "' /></td></tr><br><tr><td><b>Nazwisko:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td><td><input type='text' name='nazwisko' pattern='[A-Za-z]*' title='Nazwisko powinno składać sie tylko z liter' value='" + nazwisko +
+		   "' /></td></tr><tr><br><td><b>Numer telefonu:</b></td><td><input type='text' name='numertelefonu'  pattern='[0-9]*' title='Numer telefonu powinien składać się tylko z liczb' value='" + numertelefonu + 
+		   "' /></td></tr><tr><br><td colspan='2'><div class='zapisz'><input type='submit' value='ZAPISZ'></div></td></tr></form></div>");
+		%>
+	<br>
+	<a href="index.jsp"><h3><center><font color="green">Powrót do strony głównej</a></h3></center>
+	<h1>Edytuj Klienta</h1>
+</body>
+</html>
---
+++ target/tmp/jsp/org/apache/jsp/edytujKlienta_jsp.java
@@ -0,0 +1,150 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import main.java.com.smatyjas.domain.Klient;
+
+public final class edytujKlienta_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("<html>\r\n");
+      out.write("<head>\r\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n");
+      out.write("<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("<title>Edytuj Klienta</title>\r\n");
+      out.write("</head>\r\n");
+      out.write("\r\n");
+      out.write("<style rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("\tbody{\r\n");
+      out.write("\t\tbackground-color: #000000;\r\n");
+      out.write("\t\tbackground: url('http://www.tapeta-czarne-linie-tlo.na-telefon.org/tapety/czarne-linie-tlo.jpeg'); \r\n");
+      out.write("\t\tbackground-position: center top; no-repeat;\r\n");
+      out.write("\t}\r\n");
+      out.write("\t.przedrostki{ \r\n");
+      out.write("\t\tmargin-top: 4cm; \r\n");
+      out.write("\t\tmargin-left: 15cm; \r\n");
+      out.write("\t\tfont-size: 24px;\r\n");
+      out.write("\t\tcolor: green;\r\n");
+      out.write("\t}\r\n");
+      out.write("\t.zapisz{ \r\n");
+      out.write("\t\tmargin-left:100px; \r\n");
+      out.write("\t\tmargin-top: 20px;\r\n");
+      out.write("\t}\r\n");
+      out.write("\ta{ \r\n");
+      out.write("\t\tcolor:green; \r\n");
+      out.write("\t\tfont-size:24px; \r\n");
+      out.write("\t\tfont-weight:bold; \r\n");
+      out.write("\t\tmargin-left: 120px;\r\n");
+      out.write("\t\tmargin-right:auto;\r\n");
+      out.write("\t\tmargin-top: 2cm;\r\n");
+      out.write("\t}\r\n");
+      out.write("    h1{\r\n");
+      out.write("    \tmargin-left: 16cm;\r\n");
+      out.write("    \tcolor: green; \r\n");
+      out.write("    \tfont-size: 40px; \r\n");
+      out.write("    \tfont-style: italic;\r\n");
+      out.write("        font-family: \"Times-New-Roman\"; \r\n");
+      out.write("        margin-top: -8cm;\r\n");
+      out.write("\t}\r\n");
+      out.write("</style>\r\n");
+      out.write("\r\n");
+      out.write("<body>\r\n");
+      out.write("\t");
+      main.java.com.smatyjas.domain.Klient klient = null;
+      synchronized (session) {
+        klient = (main.java.com.smatyjas.domain.Klient) _jspx_page_context.getAttribute("klient", PageContext.SESSION_SCOPE);
+        if (klient == null){
+          klient = new main.java.com.smatyjas.domain.Klient();
+          _jspx_page_context.setAttribute("klient", klient, PageContext.SESSION_SCOPE);
+        }
+      }
+      out.write("\r\n");
+      out.write("\t");
+      org.apache.jasper.runtime.JspRuntimeLibrary.introspect(_jspx_page_context.findAttribute("klient"), request);
+      out.write("\r\n");
+      out.write("\t");
+      main.java.com.smatyjas.service.KlientManager klientmanager = null;
+      synchronized (application) {
+        klientmanager = (main.java.com.smatyjas.service.KlientManager) _jspx_page_context.getAttribute("klientmanager", PageContext.APPLICATION_SCOPE);
+        if (klientmanager == null){
+          klientmanager = new main.java.com.smatyjas.service.KlientManager();
+          _jspx_page_context.setAttribute("klientmanager", klientmanager, PageContext.APPLICATION_SCOPE);
+        }
+      }
+      out.write("\r\n");
+      out.write("\t");
+
+        String ID = request.getParameter("update");
+     	int numertelefonu = 0;
+    	String  imie = "",  nazwisko = "";
+        for (Klient k : klientmanager.pobierzKlientow()){
+          if (k.getId_klient() == Integer.parseInt(ID)){
+      		imie = k.getImie();
+       		nazwisko = k.getNazwisko();
+        	numertelefonu = k.getNumertelefonu();
+        	break;
+        	}
+        }
+   		out.println("<div class = 'przedrostki'><form action='updateKlient'><input type='hidden' name='id' value='" + ID +
+		   "' /><tr><td colspan='2'><b>ID Klienta:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;  &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  " + ID +
+		   "</td></tr><br><tr><td><b>Imie: </b> &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; </td><td><input type='text' name='imie' pattern='[A-Za-z]*' title='Imie ma powinno składać się tylko z liter' value='" + imie +
+		   "' /></td></tr><br><tr><td><b>Nazwisko:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td><td><input type='text' name='nazwisko' pattern='[A-Za-z]*' title='Nazwisko powinno składać sie tylko z liter' value='" + nazwisko +
+		   "' /></td></tr><tr><br><td><b>Numer telefonu:</b></td><td><input type='text' name='numertelefonu'  pattern='[0-9]*' title='Numer telefonu powinien składać się tylko z liczb' value='" + numertelefonu + 
+		   "' /></td></tr><tr><br><td colspan='2'><div class='zapisz'><input type='submit' value='ZAPISZ'></div></td></tr></form></div>");
+		
+      out.write("\r\n");
+      out.write("\t<br>\r\n");
+      out.write("\t<a href=\"index.jsp\"><h3><center><font color=\"green\">Powrót do strony głównej</a></h3></center>\r\n");
+      out.write("\t<h1>Edytuj Klienta</h1>\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
